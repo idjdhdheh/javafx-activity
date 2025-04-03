7ce1fcefe5e821527e17bc14bbf40877f39e28a5
ellyserrano <ellyserrano37@gmail.com>
2025-04-03 19:07:40 +0800

    added functionality to counter app.

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,4 +1,24 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CounterController {
+
+
+    @FXML
+    private Label labelcount;
+
+    @FXML
+    protected void onPlusClick() {
+        var countText = labelcount.getText();
+        var countValue = Integer.parseInt(countText);
+        countValue++;
+        labelcount.setText(countValue + "");
+    }
+
+    @FXML
+    protected void onMinusClick() {
+
+    }
 }
